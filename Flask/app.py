b228db9dd05f84db5eaeda4e481b0719418174d7
2176708c <2176708c@student.gla.ac.uk>
2017-11-28 16:09:42 +0000

    Merged Julie's uploader with our uploader and hard coded login authentication to work with 'admin'

--- Flask/app.py
+++ Flask/app.py
@@ -1,50 +1,62 @@
-
 from flask import (
-    Flask,
-    flash,
-    redirect,
-    render_template,
-    request,
-    session,
-    abort
-)
-from werkzeug import secure_filename
-import subprocess
-app = Flask(__name__)
+                   Flask,
+                   flash,
+                   redirect,
+                   render_template,
+                   request,
+                   session,
+                   abort
+                   )
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from flask.ext.login import LoginManager
 
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 @app.route('/')
-def welcome():
-    return render_template('welcome.html')
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      file_path = "static/gpx/" + secure_filename(f.filename)
-      f.save(file_path)
-      subprocess.call(['firefox', '/home/leonidas/workspace/IS/ISproject/htmlFiles/hello.html'])
-      return render_template('welcome.html')
+def home():
+    return (render_template('home.html'))
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return (render_template('about.html'))
 
-@app.route('/choose')
-def choose():
-    return render_template('choose.html')
+@app.route('/upload')
+def upload():
+    return (render_template('upload.html'))
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return (render_template('profile.html'))
 
-@app.route('/create')
-def create():
-    return render_template('create.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error=None
+    if request.method == 'POST':
+        if request.form['username']=='admin' and request.form['password'] == 'admin':
+            return (render_template('home.html'))
+        else:
+            error= 'Invalid Credentials! Please try again'
+    return render_template('login.html', error=error)
 
-@app.route('/')
-def logout():
-    return render_template('welcome.html')
+@app.route('/register',  methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        return (render_template('home.html'))
+    return (render_template('register.html'))
+
+@app.route('/julie')
+def julie():
+    return (render_template('julie.html'))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True)
+
